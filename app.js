66729db4a4c39ee2db1d31499342e56a1afb10f0
Leandro Matheus <kesuhige@gmail.com>
2021-05-19 16:40:05 -0300

    Add app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const routes = require("./src/routes")
+const app = express()
+const hostname = process.env.HOTNAME || "localhost"
+const port = process.env.PORT || 8080
+
+app.use(bodyParser.json())
+
+app.listen(port, hostname, () => {
+  console.log(`Email Api app listening at http://${hostname}:${port}`)
+})
+
+app.use("/api/v1", routes);
