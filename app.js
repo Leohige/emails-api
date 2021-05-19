2a3ce45b0ebba7d225866313e608b03505ef52ed
Leandro Matheus <kesuhige@gmail.com>
2021-05-19 16:48:12 -0300

    Fix incorrect env var HOTNAME to HOSTNAME

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const routes = require("./src/routes")
 const app = express()
-const hostname = process.env.HOTNAME || "localhost"
+const hostname = process.env.HOSTNAME || "localhost"
 const port = process.env.PORT || 8080
 
 app.use(bodyParser.json())
