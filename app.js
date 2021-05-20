dd867139563bd3a687b01e50e5ef70bbd0d9af8d
Leandro Matheus <kesuhige@gmail.com>
2021-05-20 12:55:07 -0300

    Fix CORS policy

--- app.js
+++ app.js
@@ -11,4 +11,11 @@ app.listen(port, hostname, () => {
     console.log(`Email Api app listening at http://${hostname}:${port}`)
 })
 
+app.use((_, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Content-Type")
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+    next()
+})
+
 app.use("/api/v1", routes)
